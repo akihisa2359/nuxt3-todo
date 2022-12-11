d2c9f5aaaf66ca8b14007e4418fa085810be0e6b
akihisa.makimoto <akihisa2359@gmail.com>
2022-12-11 23:58:59 +0900

    firebase用config設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   runtimeConfig: {
     apiSecret: "123",
     secretTestValue: process.env.TEST_VALUE,
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSender_id: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID,
+    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
     public: {
       apiBase: "/api",
       publicTestValue: process.env.TEST_VALUE,
