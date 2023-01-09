1ab89155e9d4a64f108091f83399ab660066f73a
akihisa.makimoto <akihisa2359@gmail.com>
2023-01-10 00:53:19 +0900

    progress bar

--- pages/test.vue
+++ pages/test.vue
@@ -4,16 +4,19 @@
     <button type="button" @click="counter++">++</button>
     <button type="button" @click="counter--">--</button>
     <NuxtLink to="/test2">to test2</NuxtLink>
-    <button type="button" @click="isToastVisible = !isToastVisible">
+    <!-- <button type="button" @click="isToastVisible = !isToastVisible">
       toast
-    </button>
+    </button> -->
+    <button type="button" @click="showToast">show toast</button>
   </div>
 </template>
 
 <script setup>
 import { useToast } from "vue-toastification";
-const toast = useToast();
-toast("hoge");
 const counter = useCounter("counter");
-const isToastVisible = useToast("isToastVisible");
+// const isToastVisible = useToast("isToastVisible");
+const showToast = () => {
+  const toast = useToast();
+  toast.success("hoge");
+};
 </script>
