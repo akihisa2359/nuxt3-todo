59559d908f548250168e567a32ae3f6ca00e5791
akihisa.makimoto <akihisa2359@gmail.com>
2023-01-12 23:59:20 +0900

    toast composable

--- composables/useToast.ts
+++ composables/useToast.ts
@@ -1,5 +1,13 @@
 import type { Ref } from "vue";
 
+enum TYPE {
+  SUCCESS = "success",
+  ERROR = "error",
+  WARNING = "warning",
+  INFO = "info",
+  DEFAULT = "default",
+}
+
 export const useToast = () => {
   // const isVisible = useState("isToastVisible", () => false);
   // return isVisible;
@@ -16,7 +24,18 @@ export const useToast = () => {
     return toasts;
   };
 
+  const toast = (content: string, type: TYPE): string => {
+    const id = String(Date.now());
+    toasts.value.push({ id: Date.now(), content, type });
+    return id;
+  };
+
+  toast.success = (content: string) => {
+    toast(content, TYPE.SUCCESS);
+  };
+
   return {
+    toast,
     toasts,
     addToast,
     removeToast,
--- pages/tests/toast.vue
+++ pages/tests/toast.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <button type="button" @click="addToast">add toast</button>
+    <button type="button" @click="addSuccessToast">add success toast</button>
   </div>
 </template>
 
 <script setup>
-const toast = useToast();
+const { toast } = useToast();
 const addToast = () => {
-  toast.addToast("hoge");
+  toast("hogeに成功しました", "success");
   console.log(toast.toasts.value);
+};
+
+const addSuccessToast = () => {
+  toast.success("hoge success");
 };
 </script>
