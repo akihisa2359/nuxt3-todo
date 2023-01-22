fb671e9baa3d2ca59dce011973d1a518d89a7454
akihisa.makimoto <akihisa2359@gmail.com>
2023-01-22 23:58:41 +0900

    toast index.ts

--- components/toast/index.ts
+++ components/toast/index.ts
@@ -1,5 +1,22 @@
-const useToast2 = () => {
-  const toast = {};
+import Toast from "./components/toastContainer.vue";
+import { EventBus } from "./libs/eventBus";
+import { createApp } from "vue";
+import { EVENTS } from "./libs/constants";
 
+const globalEventBus = new EventBus();
+
+const useToast2 = () => {
+  // const toast = {};
+  const toast = () => {};
+  toast.add = () => {
+    globalEventBus.emit(EVENTS.ADD);
+  };
   return toast;
+};
+
+const initialize = () => {
+  const app = createApp(Toast, {
+    eventBus: globalEventBus,
+  });
+  app.mount(document.createElement("div"));
 };
