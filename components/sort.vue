5be2f03a8d35049b8dcbc0ddb5eeb7a4dcf976af
akihisa.makimoto <akihisa2359@gmail.com>
2023-02-13 23:59:05 +0900

    sort_2

--- components/sort.vue
+++ components/sort.vue
@@ -2,25 +2,34 @@
   <div ref="panel" id="panel">
     <!-- <div class="indicator"> -->
     <div class="up" :style="style"></div>
-    <div class="down"></div>
+    <div class="down" :style="style"></div>
   </div>
-  <!-- </div> -->
-  <!-- {{ height }} -->
-  <!-- {{ panel?.style?.width }} -->
-  {{ style }}
 </template>
 
 <script setup>
-import { integer } from "@vee-validate/rules";
+const props = defineProps({
+  size: {
+    type: "small" | "medium" | "large",
+    default: "medium",
+  },
+});
 
 const panel = ref(null);
+const px = computed(() => {
+  switch (props.value.size) {
+    case "small":
+      return 5;
+    case "medium":
+      return 8;
+    case "large":
+      return 11;
+  }
+});
 const style = computed(() => {
-  const hoge = {
-    height: panel.value?.offsetHeight / 2,
-    border: "5px solid transparent",
+  return {
+    height: panel.value?.offsetHeight / 2 + "px",
+    border: `${px}px solid transparent`,
   };
-  return hoge;
-  // return panel.value?.offsetHeight / 2;
 });
 </script>
 
@@ -31,13 +40,15 @@ const style = computed(() => {
 .up {
   position: absolute;
   // border: 5px solid transparent;
-  border-bottom-color: #aaa;
-  bottom: 0;
+  border-bottom-color: #aaa !important;
+  bottom: 50%;
+  margin-bottom: 2px;
 }
 .down {
   position: absolute;
   // border: 5px solid transparent;
-  border-top-color: #555;
-  top: 0px;
+  border-top-color: #555 !important;
+  top: 50%;
+  margin-top: 2px;
 }
 </style>
