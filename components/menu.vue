61a68a08449ce6acd49ed1c8b50a1a4fa8f3ffd7
akihisa.makimoto <akihisa2359@gmail.com>
2023-02-16 01:09:39 +0900

    add menu

---
+++ components/menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="menu" class="container">
+    <a class="item">
+      <!-- <div> -->
+      <span class="material-symbols-outlined icon"> login </span>
+      <!-- </div> -->
+      login
+    </a>
+  </div>
+</template>
+
+<style lang="scss">
+#menu {
+  display: flex;
+  .item {
+    display: block;
+    margin-left: auto;
+    &:hover {
+      cursor: pointer;
+    }
+
+    .icon {
+      display: block;
+    }
+  }
+}
+</style>
